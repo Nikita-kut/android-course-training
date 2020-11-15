df7b47f950e6295d979ac1407de81660e8753600
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2020-11-15 11:47:01 +0300

    Home

--- KotlinBasic/02_Functions/app/src/main/java/com/example/skillbox/kotlin/funcproject/FuncProject.kt
+++ KotlinBasic/02_Functions/app/src/main/java/com/example/skillbox/kotlin/funcproject/FuncProject.kt
@@ -1,2 +1,20 @@
 package com.example.skillbox.kotlin.funcproject
 
+fun main() {
+print(multiply(b = 5))
+    print(hasChildAccess(height = 180, weight = 50, age = 13))
+    print(functionReturnAny())
+}
+
+fun multiply (a: Int=100, b: Int): Int {
+    return a * b
+}
+
+fun hasChildAccess (height: Int, weight: Int, age: Int): Boolean {
+    return height > 150 && weight > 30 && age > 10
+}
+
+fun functionReturnAny (): Any {
+return "dsd"
+
+}
