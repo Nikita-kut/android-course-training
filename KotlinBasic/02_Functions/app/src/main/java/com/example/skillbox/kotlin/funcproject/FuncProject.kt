6ee504ad2263893554d240f826573dd6b474339f
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2020-11-17 22:33:46 +0300

    Home Func Kutsyy

--- KotlinBasic/02_Functions/app/src/main/java/com/example/skillbox/kotlin/funcproject/FuncProject.kt
+++ KotlinBasic/02_Functions/app/src/main/java/com/example/skillbox/kotlin/funcproject/FuncProject.kt
@@ -1,24 +1,16 @@
 package com.example.skillbox.kotlin.funcproject
-
 import kotlin.math.sqrt
-
 fun main() {
-val solutionSum = solveEquation(a=1,b=2,c=1)
-    println("Сумма корней = $solutionSum")
+    println(solveEquation2(a =1, b =2, c =1))
 }
-
-fun solveEquation (a: Int, b: Int, c: Int): String {
-    var d: Double = (b*b-4*a*c).toDouble()//расчёт дискриминанта
-    val x1 = ((-b+sqrt(d))/2*a)//расчёт х1
-    val x2 = ((-b- sqrt(d))/2*a)//расчёт х2
-    return if (d<0) {
-        "Уравнение не имеет корней"
-    } else if (d=0) {
-        (x2).toString()
-    }
-    else {
-        (x1 + x2).toString()
+fun solveEquation2 (a: Int, b: Int, c: Int): Double? {
+    val d: Double = (b * b - (4 * a * c)).toDouble()
+    return when { a<0.0 -> null
+        else -> {
+            val x1: Double = ((-b + sqrt(d.toDouble())) / 2 * a)//расчёт х1
+            val x2: Double = ((-b - sqrt(d.toDouble())) / 2 * a)//расчёт х2
+            val sum = x1 + x2
+            sum
+        }
     }
 }
-
-
--- KotlinBasic/03_Loops_Recursion_NullableTypes/LoopsHome/app/src/main/java/com/example/skillbox/kotlin/loopshome/LoppsHome.kt
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/LoopsHome/app/src/main/java/com/example/skillbox/kotlin/loopshome/LoppsHome.kt
@@ -1,18 +1,59 @@
 package com.example.skillbox.kotlin.loopshome
 
 fun main() {
-    val maxInt2 = maxInt(15,10)
-    print (maxInt2)
-
+    println(maxInt(15,18))
+    println(calculatePrice(false))
+    println(getCar(290,true))
+    println(getDevelopPost(5))
+    println(getDevelopPost(-1))
+    println(getDevelopPost(0))
+    println(getDevelopPost(2))
+    println(getDevelopPost(3))
 }
-
-fun maxInt (a: Int, b: Int): Int {
+fun maxInt (a:Int, b:Int): Int {
     val maxValue: Int
-    if (a<b) {
+    if (a > b) {
+        maxValue=a } else {
         maxValue = b
-    } else {
-        maxValue = a
     }
     return maxValue
+}
+fun calculatePrice(booleanParam: Boolean): Int {
+    return if (booleanParam) {
+        val intermidateResult: Int = 234 + 432
+        intermidateResult +5
+    } else {
+        val intermidateResult: Int = 2 + 5
+        intermidateResult +5
+    }
+}
+fun getCar(maxSpeed: Int, hasSportMode: Boolean): String {
+    return when {
+        maxSpeed < 20 -> {
+            "Трактор"
+        }
+        maxSpeed < 60 -> {
+            "Медленная машина"
+        }
+        maxSpeed <= 200 || hasSportMode -> {
+            "Обычная машина"
+        }
+        else -> {
+            "Быстрая машина"
+        }
+    }
+}
+fun getDevelopPost(experience: Int): String {
+return if (experience<0) {
+    "Incorrect"
+} else {
+    when (experience) {
+0 -> "Intern"
+        in 1..2 -> "Junior"
+        in 3..4 -> "Middle"
+        in 5..6 -> "Senior"
+        else -> "Guru"
+    }
+}
 
 }
