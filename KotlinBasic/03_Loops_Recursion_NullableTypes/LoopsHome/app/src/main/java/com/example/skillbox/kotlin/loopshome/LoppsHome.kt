3c73f1b26324ce66f48127a6bd629a712651e0af
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2020-11-21 18:22:00 +0300

    3 home Kutsyy (ver. 1)

--- KotlinBasic/03_Loops_Recursion_NullableTypes/LoopsHome/app/src/main/java/com/example/skillbox/kotlin/loopshome/LoppsHome.kt
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/LoopsHome/app/src/main/java/com/example/skillbox/kotlin/loopshome/LoppsHome.kt
@@ -1,7 +1,15 @@
 package com.example.skillbox.kotlin.loopshome
 
 fun main() {
+    print("Введите число: ")
+    val n = readLine()?.toIntOrNull() ?: return
+    println("Вы ввели число $n")
+    println("Введите ещё $n чисел")
+    val newN = readLine()?.toIntOrNull()?: return
 
+}
+fun circle(newN: Int):Long {
 
 
 }
+
