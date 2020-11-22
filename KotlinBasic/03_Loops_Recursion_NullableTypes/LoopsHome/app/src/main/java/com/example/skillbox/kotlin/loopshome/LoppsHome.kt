245bc6287bab05642d0dd9c160453f188dbb06c5
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2020-11-22 14:37:57 +0300

    3 home Kutsyy (ver. 5)

--- KotlinBasic/03_Loops_Recursion_NullableTypes/LoopsHome/app/src/main/java/com/example/skillbox/kotlin/loopshome/LoppsHome.kt
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/LoopsHome/app/src/main/java/com/example/skillbox/kotlin/loopshome/LoppsHome.kt
@@ -1,4 +1,5 @@
 package com.example.skillbox.kotlin.loopshome
+
 import kotlin.math.abs
 
 fun main() {
@@ -6,22 +7,33 @@ fun main() {
     val n = readLine()?.toIntOrNull() ?: return
     println("Вы ввели число $n")
     println("Введите ещё $n чисел")
-    var i = 0
+    //var i = 0
     var k = 0
     var sum = 0
-    while (i < n) {
+    // while (i < n) {
+    //       println("Ввод: ")
+    //      val j = readLine()?.toIntOrNull() ?: continue
+    //      sum += j
+    //       i++
+    //      if (j >= 0) k++
+    //  }
+    for (current in 1..n) {
         println("Ввод: ")
         val j = readLine()?.toIntOrNull() ?: continue
-        sum += j
-        i++
-        if (j >= 0) k++
+        if (continue) {
+            for (j in 1..n) {
+                println("Ввод: ")
+                readLine()?.toIntOrNull() ?: break
+            }
+        }
     }
     println("Положительных чисел введено: $k")
     println("Сумма введенных чисел: $sum")
     println("Наибольший общий делитель: ${recurssion(n, sum)}")
 }
+
 tailrec fun recurssion(n: Int, sum: Int = 0): Int {
-    if (sum==0) {
+    if (sum == 0) {
         return Math.abs(n)
     } else {
         return recurssion(sum, n % sum)
