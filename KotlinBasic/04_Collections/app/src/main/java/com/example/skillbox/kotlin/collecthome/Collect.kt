2f1b7b53e2c71b03bd6e53caafdeb2574d6017a6
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2020-11-25 23:09:37 +0300

    Home Collect (ver. 1)

--- KotlinBasic/04_Collections/app/src/main/java/com/example/skillbox/kotlin/collecthome/Collect.kt
+++ KotlinBasic/04_Collections/app/src/main/java/com/example/skillbox/kotlin/collecthome/Collect.kt
@@ -10,16 +10,18 @@ fun main() {
     println("Количество уникальных номеров в списке: ${mainList.toSet().size}")
     println("Сумма длинн всех номеров: ${mainList.sumBy { it!!.length }}")
 
-    var name: MutableList<String?> = emptyList<String?>().toMutableList()
-    for (current2 in 1..mainList.size) {
-        println("Введите имя человека с номером телефона ${mainList[{current2}]}")
-        name = readLine()
+    val name: MutableList<String?> = emptyList<String?>().toMutableList()
+    val mutableMap = mutableMapOf<String, String>()
+    
+    for (current in 0 until mainList.size) {
+        println("Введите имя человека с номером телефона ${mainList[current]}")
+        name.add(readLine())
+    }
+    mutableMap.put("$mainList", "$name")
+    mutableMap.forEach {
+        println("Человек: $name. Номер телефона: $mainList")
     }
 
-    val mutableMap = mutableMapOf(
-            "Человек: ${name[0]}" to "Номер телефона: ${mainList[0]}"
-    )
-    mutableMap.forEach { println(it) }
 }
 
 fun nAndPhoneNumber(n: Int): List<String?> {
