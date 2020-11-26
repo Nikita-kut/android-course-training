76a40e796bbb9288a8093ea26c70d76dd949866d
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2020-11-26 22:03:56 +0300

    Home Collect (ver. 1)

--- KotlinBasic/04_Collections/app/src/main/java/com/example/skillbox/kotlin/collecthome/Collect.kt
+++ KotlinBasic/04_Collections/app/src/main/java/com/example/skillbox/kotlin/collecthome/Collect.kt
@@ -1,37 +1,36 @@
 package com.example.skillbox.kotlin.collecthome
 
+import android.annotation.SuppressLint
+
+@SuppressLint("NewApi")
 fun main() {
     println("Введите число: ")
     val n = readLine()?.toIntOrNull() ?: return
 
-    val mainList: MutableList<String?> = nAndPhoneNumber(n) as MutableList<String?>
+    val mainList = nAndPhoneNumber(n).toList()
     mainList.forEach { println(it) }
 
     println("Количество уникальных номеров в списке: ${mainList.toSet().size}")
-    println("Сумма длинн всех номеров: ${mainList.sumBy { it!!.length }}")
+    println("Сумма длинн всех номеров: ${mainList.sumBy { it?.length ?: 0 }}")
 
-    val name: MutableList<String?> = emptyList<String?>().toMutableList()
-    val mutableMap = mutableMapOf<String, String>()
-    
+    val name = mutableListOf<String>()
+    val mutableMap = mutableMapOf<String?, String?>()
+
     for (current in 0 until mainList.size) {
         println("Введите имя человека с номером телефона ${mainList[current]}")
-        name.add(readLine())
-    }
-    mutableMap.put("$mainList", "$name")
-    mutableMap.forEach {
-        println("Человек: $name. Номер телефона: $mainList")
+        name.add(readLine() ?: return)
+        mutableMap.put(name[current], mainList[current])
     }
 
+    mutableMap.forEach { key, value -> println("Человек $key  Номер телефона $value")  }
 }
 
 fun nAndPhoneNumber(n: Int): List<String?> {
-    val mutableList = emptyList<String?>().toMutableList()
+    val mutableList = mutableListOf<String?>()
     for (current in 1..n) {
         println("Введите номер телефона: ")
         val phoneNumber: String? = readLine()
         mutableList.add(phoneNumber)
     }
-    return mutableList.filter { it!!.startsWith("+7") }
+    return mutableList.filter { it?.startsWith("+7") ?: return emptyList() }
 }
-
-
