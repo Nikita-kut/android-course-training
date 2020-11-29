e8a07645d2965eed2229786605689812565d2af0
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2020-11-29 12:30:41 +0300

    Home Collect (ver. 1)

--- KotlinBasic/04_Collections/app/src/main/java/com/example/skillbox/kotlin/collecthome/Collect.kt
+++ KotlinBasic/04_Collections/app/src/main/java/com/example/skillbox/kotlin/collecthome/Collect.kt
@@ -1,8 +1,12 @@
 package com.example.skillbox.kotlin.collecthome
 
 import android.annotation.SuppressLint
+import java.security.Key
+import java.util.*
+import java.util.regex.Pattern
 
 @SuppressLint("NewApi")
+
 fun main() {
     println("Введите число: ")
     val n = readLine()?.toIntOrNull() ?: return
@@ -13,7 +17,7 @@ fun main() {
     println("Количество уникальных номеров в списке: ${mainList.toSet().size}")
     println("Сумма длинн всех номеров: ${mainList.sumBy { it?.length ?: 0 }}")
 
-    val name = mutableListOf<String>()
+    val name = mutableListOf<String?>()
     val mutableMap = mutableMapOf<String?, String?>()
 
     for (current in 0 until mainList.size) {
@@ -21,16 +25,18 @@ fun main() {
         name.add(readLine() ?: return)
         mutableMap.put(name[current], mainList[current])
     }
-
-    mutableMap.forEach { key, value -> println("Человек $key  Номер телефона $value")  }
+    mutableMap.toSortedMap(compareBy<String?> {it?.length}.thenBy { it })
+    mutableMap.forEach { key, value -> println("Человек $key  Номер телефона $value") }
 }
 
 fun nAndPhoneNumber(n: Int): List<String?> {
     val mutableList = mutableListOf<String?>()
     for (current in 1..n) {
         println("Введите номер телефона: ")
-        val phoneNumber: String? = readLine()
-        mutableList.add(phoneNumber)
+        val phoneNumber: CharSequence = readLine().toString()
+        if (!Pattern.matches("(\\\\+\\\\d( )?)?([-\\\\( ]\\\\d{3}[-\\\\) ])( )?\\\\d{3}-\\\\d{4}", phoneNumber)) {
+            phoneNumber.length > 10 && phoneNumber.length <= 12}
+        mutableList.add(phoneNumber.toString())
     }
     return mutableList.filter { it?.startsWith("+7") ?: return emptyList() }
 }
