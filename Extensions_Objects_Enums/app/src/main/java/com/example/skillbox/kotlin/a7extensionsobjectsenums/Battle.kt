95419be4588f14fbea363bd4272989911e1d65ba
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2021-01-01 21:52:44 +0300

    7 home (ver. 5)

--- Extensions_Objects_Enums/app/src/main/java/com/example/skillbox/kotlin/a7extensionsobjectsenums/Battle.kt
+++ Extensions_Objects_Enums/app/src/main/java/com/example/skillbox/kotlin/a7extensionsobjectsenums/Battle.kt
@@ -1,13 +1,15 @@
 package com.example.skillbox.kotlin.a7extensionsobjectsenums
 
 class Battle(
-        val firstTeam: Team,
-        val secondTeam: Team,
-        val battleIsOver: Boolean
 ) {
+    val firstTeam: Team = Team()
+    val secondTeam: Team = Team()
+    val battleIsOver: Boolean = false
 
     fun getBattleState(): BattleState {
+        if (firstTeam.team.forEach { it.isKilled } == ) {
 
+        }
 
         return BattleState.Progress()
     }
