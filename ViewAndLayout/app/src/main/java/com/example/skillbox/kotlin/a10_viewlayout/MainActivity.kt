356b4adeff881a1eaa624e11264634e2cafe1335
Kutcyi Nikita <kutcyi.nikita@gmail.com>
2021-01-11 18:27:53 +0300

    10_Test

---
+++ ViewAndLayout/app/src/main/java/com/example/skillbox/kotlin/a10_viewlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.skillbox.kotlin.a10_viewlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
