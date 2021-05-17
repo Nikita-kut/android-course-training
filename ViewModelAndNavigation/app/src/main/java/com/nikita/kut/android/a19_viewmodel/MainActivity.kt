bab516bb1226b3c7d176f147360294fa43bceafb
Kutsyy Nikita <kutcyi.nikita@gmail.com>
2021-05-17 09:48:39 +0300

    19_viewmodel_navigation | initial commit

---
+++ ViewModelAndNavigation/app/src/main/java/com/nikita/kut/android/a19_viewmodel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nikita.kut.android.a19_viewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
